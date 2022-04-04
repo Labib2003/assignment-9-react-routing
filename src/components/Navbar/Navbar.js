fba630c4bc43e7eb4bbd25a4108a9b9f3ba9fb8b
Labib2003 <labib.salimi30@gmail.com>
2022-04-04 13:03:17 +0600

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <p className='text-5xl'>This is nav</p>
+        </div>
+    );
+};
+
+export default Navbar;
